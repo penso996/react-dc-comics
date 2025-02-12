98f73a0aa4f46f5559d359d3c35500705697b578
penso996 <penso.andrea@outlook.it>
2025-02-12 17:37:52 +0100

    working on main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,36 @@
 // Main.jsx
 export default function Main() {
-    return (<main>
-
-    </main>
+    return (
+        <main>
+            <div className="main-content-background">
+                <div className="main-content">
+                    <h1>-- Content Goes Here --</h1>
+                </div>
+            </div>
+            <div className="main-links-background">
+                <div className="main-links">
+                    <div className="main-links-card">
+                        <img src="\img\buy-comics-digital-comics.png" alt="" />
+                        <h3>DIGITAL COMICS</h3>
+                    </div>
+                    <div className="main-links-card">
+                        <img src="\img\buy-comics-merchandise.png" alt="" />
+                        <h3>DC MERCANDISE</h3>
+                    </div>
+                    <div className="main-links-card">
+                        <img src="\img\buy-comics-subscriptions.png" alt="" />
+                        <h3>SUBSCRIPTIONS</h3>
+                    </div>
+                    <div className="main-links-card">
+                        <img src="\img\buy-comics-shop-locator.png" alt="" />
+                        <h3>COMIC SHOP LOCATOR</h3>
+                    </div>
+                    <div className="main-links-card">
+                        <img src="\img\buy-dc-power-visa.svg" alt="" />
+                        <h3>DC POWER VISA</h3>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
