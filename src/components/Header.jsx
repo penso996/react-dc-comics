60250dc9fd0a9ed8af8f8bf36fc29f645ad9595d
penso996 <penso.andrea@outlook.it>
2025-02-12 16:59:33 +0100

    fixed header syntax

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,24 @@
 // Header.jsx
 export default function Header() {
-    return (<header>
-        <div className="header-size">
-            <div>
-                <img className="logo" src="\img\dc-logo.png" alt="logo" />
+    return (
+        <header>
+            <div className="header-size">
+                <div>
+                    <img className="logo" src="\img\dc-logo.png" alt="logo" />
+                </div>
+                <nav className="header-nav">
+                    <a href="#">CHARACTERS</a>
+                    <a className="active" href="#">COMICS</a>
+                    <a href="#">MOVIES</a>
+                    <a href="#">TV</a>
+                    <a href="#">GAMES</a>
+                    <a href="#">COLLECTIBLES</a>
+                    <a href="#">VIDEOS</a>
+                    <a href="#">FANS</a>
+                    <a href="#">NEWS</a>
+                    <a href="#">SHOP</a>
+                </nav>
             </div>
-            <nav className="header-nav">
-                <a href="#">CHARACTERS</a>
-                <a className="active" href="#">COMICS</a>
-                <a href="#">MOVIES</a>
-                <a href="#">TV</a>
-                <a href="#">GAMES</a>
-                <a href="#">COLLECTIBLES</a>
-                <a href="#">VIDEOS</a>
-                <a href="#">FANS</a>
-                <a href="#">NEWS</a>
-                <a href="#">SHOP</a>
-            </nav>
-        </div>
-    </header>
+        </header>
     );
 }
