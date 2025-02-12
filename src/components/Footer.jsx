e0f7e3f0d369f47de731758878821ba8ccf2d063
penso996 <penso.andrea@outlook.it>
2025-02-12 18:07:39 +0100

    working on footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,24 @@
 // Footer.jsx
 export default function Footer() {
-    return (<footer><h3>Footer</h3></footer>);
+    return (
+        <footer>
+            <div className="footer-background">
+                <div className="footer-container">
+                    <div className="footer-links">
+                        <h4>DC COMICS</h4>
+                        <h4>SHOP</h4>
+                        <h4>DC</h4>
+                        <h4>SITES</h4>
+                    </div>
+                    <div className="footer-logo">
+                        <img src="\img\dc-logo-bg.png" alt="logo" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+
+            </div>
+        </footer>
+    );
 }
