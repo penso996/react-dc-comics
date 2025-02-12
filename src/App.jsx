d239e2b553a4a2bb5b0f1d6534e0d242dd88fed6
penso996 <penso.andrea@outlook.it>
2025-02-12 15:17:21 +0100

    moved files in correct folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
