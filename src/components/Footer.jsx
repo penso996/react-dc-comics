40a3fe94f5d95180e7e2611195bbdac4accd3459
penso996 <penso.andrea@outlook.it>
2025-02-13 15:14:57 +0100

    working on footer both css and jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,30 @@
 export default function Footer() {
     return (
         <footer>
-            <div className="footer-background">
-                <div className="footer-container">
+
+            <div className="upper-footer-container-background">
+                <div className="upper-footer-container">
+
                     <div className="footer-links">
                         <h4>DC COMICS</h4>
                         <h4>SHOP</h4>
                         <h4>DC</h4>
                         <h4>SITES</h4>
                     </div>
-                    <div className="footer-logo">
+
+                    <div>
                         <img src="\img\dc-logo-bg.png" alt="logo" />
                     </div>
+
                 </div>
             </div>
 
-            <div className="footer-bottom">
+            <div className="lower-footer-container-background">
+                <div className="lower-footer-container">
 
+                </div>
             </div>
+
         </footer>
     );
 }
