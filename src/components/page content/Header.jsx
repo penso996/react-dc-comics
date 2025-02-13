bc550f28bac300398db3ea5f167a3c45edac8e0a
penso996 <penso.andrea@outlook.it>
2025-02-13 16:28:27 +0100

    centralized nav

--- src/components/page content/Header.jsx
+++ src/components/page content/Header.jsx
@@ -1,18 +1,7 @@
 // Header.jsx
-export default function Header() {
+import HeaderNav from '../single_components/HeaderNav'
 
-    const links = [
-        { id: 1, text: 'CHARACTERS', url: '#', current: false },
-        { id: 2, text: 'COMICS', url: '#', current: false },
-        { id: 2, text: 'MOVIES', url: '#', current: false },
-        { id: 4, text: 'TV', url: '#', current: false },
-        { id: 5, text: 'GAMES', url: '#', current: true },
-        { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
-        { id: 7, text: 'VIDEOS', url: '#', current: false },
-        { id: 8, text: 'FANS', url: '#', current: false },
-        { id: 9, text: 'NEWS', url: '#', current: false },
-        { id: 10, text: 'SHOP', url: '#', current: false },
-    ]
+export default function Header() {
 
     return (
         <header>
@@ -22,15 +11,7 @@ export default function Header() {
                     <img className="logo" src="\img\dc-logo.png" alt="logo" />
                 </div>
 
-                <nav className="header-nav">
-                    {links.map((link) => (
-                        <li key={link.id}>
-                            <a href={link.url} className={link.current ? 'active' : ''}>
-                                {link.text}
-                            </a>
-                        </li>
-                    ))}
-                </nav>
+                <HeaderNav />
 
             </div>
         </header>
---
+++ src/components/single_components/HeaderNav.jsx
@@ -0,0 +1,28 @@
+// HeaderNav.jsx
+
+const links = [
+    { id: 1, text: 'CHARACTERS', url: '#', current: false },
+    { id: 2, text: 'COMICS', url: '#', current: false },
+    { id: 2, text: 'MOVIES', url: '#', current: false },
+    { id: 4, text: 'TV', url: '#', current: false },
+    { id: 5, text: 'GAMES', url: '#', current: true },
+    { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
+    { id: 7, text: 'VIDEOS', url: '#', current: false },
+    { id: 8, text: 'FANS', url: '#', current: false },
+    { id: 9, text: 'NEWS', url: '#', current: false },
+    { id: 10, text: 'SHOP', url: '#', current: false },
+]
+
+export default function HeaderNav() {
+    return (
+        <nav className="header-nav">
+            {links.map((link) => (
+                <li key={link.id}>
+                    <a href={link.url} className={link.current ? 'active' : ''}>
+                        {link.text}
+                    </a>
+                </li>
+            ))}
+        </nav>
+    );
+}
