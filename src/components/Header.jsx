45a7bc67b39967146fe588fb393d3bbe1fabd06d
penso996 <penso.andrea@outlook.it>
2025-02-13 14:23:08 +0100

    working on header both css and jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@
 export default function Header() {
     return (
         <header>
-            <div className="header-size">
+            <div className="header-container">
+
                 <div>
                     <img className="logo" src="\img\dc-logo.png" alt="logo" />
                 </div>
+
                 <nav className="header-nav">
                     <a href="#">CHARACTERS</a>
                     <a href="#">COMICS</a>
@@ -18,6 +20,7 @@ export default function Header() {
                     <a href="#">NEWS</a>
                     <a href="#">SHOP</a>
                 </nav>
+
             </div>
         </header>
     );
