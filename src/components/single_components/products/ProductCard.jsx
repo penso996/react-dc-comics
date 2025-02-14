2285317108da8fd616422d43e1fff893f056dd63
penso996 <penso.andrea@outlook.it>
2025-02-14 17:50:51 +0100

    added products in product list

--- src/components/single_components/products/ProductCard.jsx
+++ src/components/single_components/products/ProductCard.jsx
@@ -1,7 +1,9 @@
 // ProductCard.jsx
-export default function ProductCard() {
-
+export default function ProductCard(props) {
     return (
-        <div className="product-card"></div>
+        <div className="product-card">
+            <img src={props.comic.thumb} alt={props.comic.series} />
+            <h4>{props.comic.series}</h4>
+        </div>
     );
 }
