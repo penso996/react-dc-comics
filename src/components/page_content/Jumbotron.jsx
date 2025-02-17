f1e2bdb3a59b726efc759d55eb0f890e1b3b485b
penso996 <penso.andrea@outlook.it>
2025-02-17 14:18:59 +0100

    working on jumbotron

--- src/components/page_content/Jumbotron.jsx
+++ src/components/page_content/Jumbotron.jsx
@@ -1,8 +1,10 @@
 // Jumbotron.jsx
 export default function Jumbotron() {
     return (
-        <section className="jumbotron">
-            <button>CURRENT SERIES</button>
+        <section className="jumbotron-background">
+            <section className="jumbotron-container">
+                <p>CURRENT SERIES</p>
+            </section>
         </section>
     );
 }
